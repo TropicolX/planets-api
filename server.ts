85247c5bbe6b5a5128109f0dd12e57f472164655
TropicolX <jacobsbusayo@gmail.com>
2023-08-17 10:59:28 +0100

    chore: spacing

--- server.ts
+++ server.ts
@@ -2,6 +2,7 @@ import express from "express";
 import axios from "axios";
 import dotenv from "dotenv";
 import cors from "cors";
+
 import { planets } from "./data";
 
 const PORT = process.env.PORT || 3000;
